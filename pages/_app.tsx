70376f146af68154908fe677ddca4b9a756c9504
frivolta <rivoltafilippo@gmail.com>
2022-03-24 21:08:28 +0100

    auth0 implementation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,29 @@
 import {CategoriesDrawerProvider} from "../widgets/CategoriesDrawer/hooks/useCategoriesDrawer";
 import {TransactionDrawerProvider} from "../widgets/TransactionsDrawer/hooks/userTransactionsDrawer";
-import {MonthlyFilterProvider} from "../widgets/TransactionsFilters/hooks/useTransactionsDate";
+import {Auth0Provider} from "@auth0/auth0-react";
+import Router from 'next/router';
 
 require('../styles/global.less')
+const onRedirectCallback = (appState) => {
+    Router.replace(appState?.returnTo || '/');
+};
 
 function MyApp({Component, pageProps}) {
     return (
+        <Auth0Provider
+            domain={process.env.NEXT_PUBLIC_DOMAIN}
+            clientId={process.env.NEXT_PUBLIC_CLIENT_ID}
+            audience="https://dev-iv8n6772.us.auth0.com/api/v2/"
+            scope="read:current_user update:current_user_metadata"
+            redirectUri={typeof window !== 'undefined' && window.location.origin}
+            onRedirectCallback={onRedirectCallback}
+        >
             <CategoriesDrawerProvider>
                 <TransactionDrawerProvider>
                     <Component {...pageProps} />
                 </TransactionDrawerProvider>
             </CategoriesDrawerProvider>
+        </Auth0Provider>
     );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,34 @@ import {Col, Row} from "antd";
 import {AmountDisplay} from "../components/AmountDisplay";
 import {TransactionEntriesProvider} from "../hooks/useTransactionEntries";
 import {DashboardData} from "../widgets/DashboardData";
+import {useAuth0} from "@auth0/auth0-react";
 
 const IndexPage = () => {
     const {total, apiState} = useAllTransactions()
+    const {
+        isLoading,
+        isAuthenticated,
+        error,
+        user,
+        loginWithRedirect,
+        logout,
+        getAccessTokenSilently,
+        getIdTokenClaims
+    } = useAuth0();
+    const at = async () => {
+        const token = await getAccessTokenSilently()
+        console.log(token)
+    }
+
+
     return (
         <MonthlyFilterProvider>
             <TransactionEntriesProvider>
                 <PageLayout title="Dashboard" subtitle="Review your reports" noPaper>
+                    <button onClick={() => loginWithRedirect()}>Log In</button>
+                    <button onClick={at}>Get</button>
+
+
                     <Row className="site-layout-background" style={{padding: 16, marginBottom: 16}}>
                         <Col>
                             <AmountDisplay
